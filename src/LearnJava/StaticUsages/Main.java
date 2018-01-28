e27a3e1434354932f30638e008c514fe283abbc6
Vamsi Tallapudi <vamsi@coderefer.com>
2018-01-28 12:12:44 +0530

    static method eg.

---
+++ src/LearnJava/StaticUsages/Main.java
@@ -0,0 +1,14 @@
+package LearnJava.StaticUsages;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> itemsPrice = new ArrayList<>();
+        itemsPrice.add(99);
+        itemsPrice.add(150);
+        itemsPrice.add(45);
+        System.out.println(Util.addItemsRateFromCart(itemsPrice));
+    }
+}
