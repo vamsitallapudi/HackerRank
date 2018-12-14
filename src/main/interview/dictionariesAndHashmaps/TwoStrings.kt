c6abbeab05a15bd9b99c6f78f187809b99126adc
Vamsi Tallapudi <vamsi@coderefer.com>
2018-12-14 10:22:29 +0530

    Two Strings Solution but with Time out for few test cases

---
+++ src/main/interview/dictionariesAndHashmaps/TwoStrings.kt
@@ -0,0 +1,39 @@
+package main.interview.dictionariesAndHashmaps
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    var count = sc.nextInt()
+    sc.nextLine()
+
+    while (count-- > 0) {
+        val string1 = sc.nextLine()
+        val string2 = sc.nextLine()
+        if(containsCommonSubstring(string1, string2)) println("YES") else println("NO")
+    }
+}
+
+fun containsCommonSubstring(str1:String, str2:String): Boolean {
+    val str1SubStringsList = fetchSubStringList(str1)
+    val str2SubStringsList = fetchSubStringList(str2)
+    for (i in 0 until str2SubStringsList.size) {
+        if(str1SubStringsList.contains(str2SubStringsList[i])) {
+            return true
+        } else {
+            continue
+        }
+    }
+    return false
+}
+
+fun fetchSubStringList(str: String) : List<String> {
+    var strList = ArrayList<String>()
+    for(i in 0 until str.length) {
+        for(j in i+1 .. str.length) {
+            strList.add(str.substring(i,j))
+        }
+    }
+    return strList
+}
