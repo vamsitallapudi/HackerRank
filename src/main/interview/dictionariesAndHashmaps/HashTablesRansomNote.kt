c9c3f423635e1c93dc21c5b80d450cee187c8052
Vamsi Tallapudi <vamsi@coderefer.com>
2018-12-14 10:21:32 +0530

    Ransom Note accepted solution

--- src/main/interview/dictionariesAndHashmaps/HashTablesRansomNote.kt
+++ src/main/interview/dictionariesAndHashmaps/HashTablesRansomNote.kt
@@ -3,25 +3,43 @@ package main.interview.dictionariesAndHashmaps
 fun main(args: Array<String>) {
     val sizes = readLine()!!.trim().split(" ").map { it.toInt() }
 
-    val magazine = HashMap<String, Int>(sizes[0])
-    val ransomLine = HashMap<String, Int>(sizes[1])
+    val magazineMap = HashMap<String, Int>(sizes[0])
+    val ransomMap = HashMap<String, Int>(sizes[1])
 
     val magWords = readLine()!!.trim().split(" ").toTypedArray()
     val ransomWords = readLine()!!.trim().split(" ").toTypedArray()
 
     for(i in 0 until magWords.size) {
-        if(magazine.containsKey(magWords[i])) {
-            magazine[magWords[i]] = magazine.getValue(magWords[i]) + 1
+        if(magazineMap.containsKey(magWords[i])) {
+            magazineMap[magWords[i]] = magazineMap.getValue(magWords[i]) + 1
         } else {
-            magazine[magWords[i]] = 1
+            magazineMap[magWords[i]] = 1
         }
     }
     for(i in 0 until ransomWords.size) {
-        if(ransomLine.containsKey(ransomWords[i])) {
-            ransomLine[ransomWords[i]] = ransomLine.getValue(ransomWords[i]) + 1
+        if(ransomMap.containsKey(ransomWords[i])) {
+            ransomMap[ransomWords[i]] = ransomMap.getValue(ransomWords[i]) + 1
         } else {
-            ransomLine[ransomWords[i]] = 1
+            ransomMap[ransomWords[i]] = 1
         }
     }
-    println(magazine)
+    if(checkIfPossible(ransomMap, magazineMap))
+        println("Yes")
+    else
+        println("No")
+}
+
+private fun checkIfPossible(ransomMap: HashMap<String, Int>, magazineMap: HashMap<String, Int>): Boolean {
+    for (i in ransomMap.entries) {
+        return if (magazineMap.containsKey(i.key)) {
+            if(magazineMap[i.key]!! - ransomMap[i.key]!! >= 0) {
+                continue
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
+    return true
 }
